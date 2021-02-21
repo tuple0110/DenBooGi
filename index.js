1d46441d4260f6fb7d737f0c47b89c41347a6901
unknown <>
2021-02-21 22:36:57 +0900

    hosting

--- index.js
+++ index.js
@@ -21,33 +21,33 @@ let driver;
 let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
 
 func = async () => {
-    driver = await new webdriver.Builder()
-    .forBrowser(webdriver.Browser.CHROME)
-    .setChromeOptions(options)
-    .setChromeService(serviceBuilder)
-    .build();
-    await driver.get('http://denvers.kro.kr:8123/');
-    // try {
-    //     await driver.wait(webdriver.until.elementLocated(webdriver.By.className("messagelist")), Infinity);
-    //     var old = "";
-    //     var message = "";
-    //     var messageRow;
-    //     while (true) {
-    //         await driver.wait(async () => {
-    //             messageRow = await driver.findElements(webdriver.By.className("messagerow"));
-    //             if (messageRow.length > 0) {
-    //                 message = await messageRow[messageRow.length - 1].getText();
-    //                 return message != old;
-    //             } else {
-    //                 return false;
-    //             }
-    //         }, Infinity);
-    //         old = message;
-    //         channel.send(message.replace("보리님", "<@353859042583642122>"));
-    //     }
-    // } catch (e) {
-    //     driver.quit();
-    //     func();
-    // }
+    try {
+        driver = await new webdriver.Builder()
+        .forBrowser(webdriver.Browser.CHROME)
+        .setChromeOptions(options)
+        .setChromeService(serviceBuilder)
+        .build();
+        await driver.get('http://denvers.kro.kr:8123/');
+        await driver.wait(webdriver.until.elementLocated(webdriver.By.className("messagelist")), Infinity);
+        var old = "";
+        var message = "";
+        var messageRow;
+        while (true) {
+            await driver.wait(async () => {
+                messageRow = await driver.findElements(webdriver.By.className("messagerow"));
+                if (messageRow.length > 0) {
+                    message = await messageRow[messageRow.length - 1].getText();
+                    return message != old;
+                } else {
+                    return false;
+                }
+            }, Infinity);
+            old = message;
+            channel.send(message.replace("보리님", "<@353859042583642122>"));
+        }
+    } catch (e) {
+        driver.quit();
+        func();
+    }
 }
 client.login(process.env.BOT_TOKEN);
