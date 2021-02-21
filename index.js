7b5aa2f3a4fff935ceb815fc0234bd84ae82d67a
unknown <>
2021-02-21 20:56:52 +0900

    hosting

--- index.js
+++ index.js
@@ -14,31 +14,30 @@ let driver;
 
 func = async () => {
     driver = await new webdriver.Builder()
-    try {
-        driver = await new webdriver.Builder()
-        .forBrowser('chrome')
-        .build();
-        await driver.get('http://denvers.kro.kr:8123/');
-        await driver.wait(webdriver.until.elementLocated(webdriver.By.className("messagelist")), Infinity);
-        var old = "";
-        var message = "";
-        var messageRow;
-        while (true) {
-            await driver.wait(async () => {
-                messageRow = await driver.findElements(webdriver.By.className("messagerow"));
-                if (messageRow.length > 0) {
-                    message = await messageRow[messageRow.length - 1].getText();
-                    return message != old;
-                } else {
-                    return false;
-                }
-            }, Infinity);
-            old = message;
-            channel.send(message.replace("보리님", "<@353859042583642122>"));
-        }
-    } catch (e) {
-        driver.quit();
-        func();
-    }
+    .forBrowser('chrome')
+    .build();
+    await driver.get('http://denvers.kro.kr:8123/');
+    // try {
+    //     await driver.wait(webdriver.until.elementLocated(webdriver.By.className("messagelist")), Infinity);
+    //     var old = "";
+    //     var message = "";
+    //     var messageRow;
+    //     while (true) {
+    //         await driver.wait(async () => {
+    //             messageRow = await driver.findElements(webdriver.By.className("messagerow"));
+    //             if (messageRow.length > 0) {
+    //                 message = await messageRow[messageRow.length - 1].getText();
+    //                 return message != old;
+    //             } else {
+    //                 return false;
+    //             }
+    //         }, Infinity);
+    //         old = message;
+    //         channel.send(message.replace("보리님", "<@353859042583642122>"));
+    //     }
+    // } catch (e) {
+    //     driver.quit();
+    //     func();
+    // }
 }
 client.login(process.env.BOT_TOKEN);
